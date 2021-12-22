a79ad858f805eecd0eb6dd18de82e30158c10e4d
Tanishq Deshmukh <tanishqdeshmukh30@gmail.com>
2021-12-22 10:59:29 +0000

    Added sword and jump sound

--- assets/player/player.go
+++ assets/player/player.go
@@ -74,3 +74,49 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sword"
+  type: "sound"
+  data: "sound: \"/assets/player/sound/sword.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "jump"
+  type: "sound"
+  data: "sound: \"/assets/player/sound/jump.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
